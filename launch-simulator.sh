08993706816bf8611f1d587db00b6abd75e932b3
Mike Branstein <mike.branstein@gmail.com>
2016-07-30 21:36:50 -0400

    added command-line argument to specify output filename

--- launch-simulator.sh
+++ launch-simulator.sh
@@ -1,5 +1,23 @@
 #!/bin/bash 
 
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -f|--filename)
+    FILENAME="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+echo $FILENAME
+
 sim="./start-simulator.sh"
 $sim &
 
@@ -9,11 +27,11 @@ echo $SIM_PID
 sleep 45
 
 screenshot="/Applications/iOS-Simulator Cropper.app/Contents/MacOS/iOS-Simulator Cropper"
-options=( -"p" "2" -"f" "/Users/mike/Desktop/screenshot.png" )
+options=( -"p" "2" -"f" "/Users/mike/Desktop/screenshot-$FILENAME.png" )
 command=( "$screenshot" "${options[@]}" )
 
 # execute it:
 "${command[@]}"
 
 # execute it:
-kill $(jobs -p)
+'kill $(jobs -p)'
