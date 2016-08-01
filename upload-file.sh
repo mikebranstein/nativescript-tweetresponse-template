e77e4e4177f152eea022d7e2c68be3bdcd6dfca9
Mike Branstein <mike.branstein@gmail.com>
2016-08-01 14:53:10 -0400

    fix notification to tweet

--- upload-file.sh
+++ upload-file.sh
@@ -1,7 +1,49 @@
 #!/bin/bash 
 
-image="$(cat /Users/mike/Desktop/screenshot-__Name__.png | base64)"
+# BEGIN COMMAND LINE ARGS PARSING
+while [[ $# -gt 1 ]]
+do
+key="$1"
 
-body='{"tweetid":"__TweetId__","image":"'$image'"}'
+case $key in
+    -f|--filename)
+    FILENAME="$2"
+    shift # past argument
+    ;;
+    -t|--tweetid)
+    TWEETID="$2"
+    shift #past argument
+    ;;
+    -u|--username)
+    USERNAME="$2"
+    shift #past argument
+    ;;
+    -p|--password)
+    PASSWORD="$2"
+    shift #past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+# END COMMAND LINE ARGS PARSING
 
-curl -H "Content-Type: application/json" -X POST -d $body -u __BasicAuthUsername__:__BasicAuthPassword__ https://nstweet.brosteins.com/api/tweet
+echo "FILENAME = "$FILENAME
+echo "TWEETID = "$TWEETID
+echo "USERNAME = "$USERNAME
+echo "PASSWORD = "$PASSWORD
+
+image="$(cat /Users/mike/Desktop/screenshot-"$FILENAME".png | base64)"
+auth="$(echo -n "$USERNAME":"$PASSWORD" | base64)"
+
+body='{"tweetid":"'$TWEETID'","image":"'$image'"}'
+
+echo="echo"
+httpie="http"
+options=( "POST" "https://nstweet.brosteins.com/api/tweet" "body='$body'"  --"auth" "$USERNAME:$PASSWORD" --"json" )
+command=( "$httpie" "${options[@]}" )
+
+# execute it:
+"${command[@]}"
